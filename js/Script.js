6798500fe045d334ecc13d78bcbfa0cc05ac1023
Jose Julian Alvarez Uparela <josejulianalvarezu@gmail.com>
2026-01-05 20:26:02 -0500

    Iniciamos con toda :)

---
+++ js/Script.js
@@ -0,0 +1,21 @@
+// Mi primera animacion
+gsap.to(".Caja-1", {duration: 2, x: 100});
+
+// Mi segunda animacion vertical
+gsap.to(".Caja-2", {duration: 2 , y: 100, delay: 2})
+
+// Mi tercera animacion con de derecha a izquierda
+gsap.to(".Caja-3", {duracion:2, x: -100, delay: 3})
+
+
+// Mi cuarta animacion con abajo hacia arriba
+gsap.to(".Caja-4", {duration: 1, y: -100, delay: 4})
+
+
+
+
+
+
+
+
+
